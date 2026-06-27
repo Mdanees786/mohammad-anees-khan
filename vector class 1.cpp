0aafefa35650510a7937669fbc06984ee4f0052e
Mdanees786 <mohammadanish14035@gmail.com>
2026-06-27 13:21:06 +0530

    Add files via upload

--- vector class 1.cpp
+++ vector class 1.cpp
@@ -2,71 +2,106 @@
 using namespace std;
 
 int main() {
-	//Create a vector and display all elements
-	vector<int>vec={1,2,3,4,5,6};
-	for(int i=0;i<vec.size();i++){
-	    cout<<vec[i]<<" ";
-	}
-	//Insert elements in vector using push_back() and print size
-	
-	
-	vec.push_back(10);
-	cout<<endl;
-		for(int i=0;i<vec.size();i++){
-	    cout<<vec[i]<<" ";
-	}
-	cout<<endl;
-	cout<<vec.size()<<endl;
-	
-//	Remove last element using pop_back()
 
-vec.pop_back();
-for(int i=0;i<vec.size();i++){
-	    cout<<vec[i]<<" ";
+ //Print all elements of a vector.
+ int n;
+ cin>>n;
+ vector<int>vec(n);
+ for(int i=0;i<n;i++){
+     cin>>vec[i];
+}
+for(int i=0;i<n;i++){
+    cout<<vec[i]<<" ";
 }
 cout<<endl;
 
-  //Find size and capacity of a vector
-  
-  cout<<vec.size()<<" "<<vec.capacity()<<endl;
-  
-   //Take n numbers input in vector and find sum of elements
-   
-   int n;
-   cin>>n;
-  int sum=0;
-   vector<int>vec2;
-   for(int i=0;i<n;i++){
-       cin>>vec[i];
-        
-       sum+=vec[i];
-       
-   }
-   cout<<sum<<endl;
-   
-   //Find maximum and minimum element in vector
-   
+//Find the sum of all elements.
 
-    int m;
-    cin >> m;
-
-    vector<int> vec1(m);
-
-    for(int i=0; i<m; i++){
-        cin >> vec1[i];
-    }
-
-    int max = vec[0];
-
-    for(int i=1; i<m; i++){
-        if(max < vec1[i]){
-            max = vec1[i];
-        }
-    }
-
-    cout << "Maximum = " << max << endl;
-    
-    
-  
-	return 0;
+int sum =0;
+for(int i=0;i<n;i++){
+    sum += vec[i];
 }
+cout<<"sum of element"<<sum<<endl;
+
+//Find the average of elements.
+
+double avg = sum/n;
+cout<<"average"<<avg<<endl;
+
+//Find the maximum element.
+
+int max = vec[0];
+for(int i=1;i<n;i++){
+    if(max<vec[i]){
+        max= vec[i];
+    }
+}
+cout<<"max element"<<max<<endl;
+
+
+//Find the minimum element.
+int min = vec[0];
+for(int i=1;i<n;i++){
+    if(min>vec[i]){
+        min= vec[i];
+    }
+}
+cout<<"min element"<<min<<endl;
+
+//Count even and odd numbers.
+
+int even =0,odd=0;
+for(int i=0;i<n;i++){
+    if(vec[i]%2==0){
+        even++;
+    }
+    else{
+        odd++;
+    }
+    
+}
+cout<<"even count"<<" "<<even<<endl;
+cout<<"odd count"<<" "<<odd<<endl;
+
+//Search an element.
+
+int key;
+cin>>key;
+bool found= false;
+for(int i=0;i<n;i++){
+    if(key==vec[i]){
+        found= true;
+        break;
+    }
+    
+}
+if(found){
+    cout<<"preset"<<endl;
+}
+else{
+    cout<<"not present "<<endl;
+}
+
+
+//Count occurrences of an element.
+
+
+int x;
+cin>>x;
+int count =0;
+for(int i=0;i<n;i++){
+    if(x==vec[i]){
+        count++;   
+        }
+}
+cout<<"x count "<<" "<<count<<endl;
+
+//Reverse a vector.
+
+
+for(int i=n-1;i>=0;i--){
+    cout<<vec[i]<<" ";
+}
+
+}
+
